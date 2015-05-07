c3231a3836cd1786c49916ee896cf94f3393c9cd
tuannv-asiantech <tuan08t2@gmail.com>
2015-05-07 13:38:59 +0700

    upload code

---
+++ app/src/main/java/com/example/vantuan/materialdesign/activity/FriendsFragment.java
@@ -0,0 +1,49 @@
+package com.example.vantuan.materialdesign.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.vantuan.materialdesign.R;
+
+/**
+ * Created by vantuan on 06/05/2015.
+ */
+public class FriendsFragment extends Fragment
+{
+    public FriendsFragment()
+    {
+        super();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach()
+    {
+        super.onDetach();
+    }
+}
